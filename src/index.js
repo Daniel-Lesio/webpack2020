46c45a5e77ed2b66586b047f8a70ddb00240a98a
Daniel Lesio <lesiewiczsales@gmail.com>
2020-03-11 13:07:37 +0100

    basic

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/main.scss'
+const arr = [1,2,3]
+
+const arr2 = [...arr , 4]
+console.log(arr2)
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    module : {
+        rules : [
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : "babel-loader"
+                }
+            },
+            {
+                test : /\.html$/,
+                use : [
+                    {
+                     loader : "html-loader",
+                     options : {minimize :false}   
+                    }
+                ]
+            },
+            {
+                test : /\.(png|svg|jpeg|jpg|gif)$/i,
+                loader : 'file-loader',
+                options : {
+                    name : 'assets/[name].[ext]',
+                    esModule: false,
+                }
+            },
+            {
+                test : /\.scss$/,
+                use : [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins : [
+        new htmlWebpackPlugin({
+            template : "./src/index.html",
+            filename : "./index.html",
+        }),
+        new miniCssExtractPlugin({
+            filename : "[name].css",
+            chunkFilename : "[id].css"
+        })
+    ]
+}
